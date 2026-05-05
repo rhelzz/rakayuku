b0ab359474127dca67c5912f387255e9e886d3fe
rhelzz <muhammadrasyad1004@gmail.com>
2026-05-06 04:40:55 +0700

    feat(stock-movement): add Searchable trait to StockMovement model

--- app/Models/StockMovement.php
+++ app/Models/StockMovement.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\Searchable;
 use Illuminate\Database\Eloquent\Model;
 
 class StockMovement extends Model
 {
+    use Searchable;
+
     protected $guarded = ['id'];
 
     public function material()
